242fcfe02f7403dada1126a6d8d88b45cfa3a82b
Andres <felipem1145@gmail.com>
2020-02-05 19:49:41 -0500

    aPP

---
+++ ContactsApp/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace ContactsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFind = new System.Windows.Forms.Button();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtIDremove = new System.Windows.Forms.TextBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(265, 38);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(75, 23);
+            this.btnFind.TabIndex = 0;
+            this.btnFind.Text = "Find";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.BtnFind_Click);
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(130, 38);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 20);
+            this.txtID.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ID";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(130, 118);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(100, 20);
+            this.txtFirstName.TabIndex = 3;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(130, 159);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(100, 20);
+            this.txtLastName.TabIndex = 4;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(130, 205);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(100, 20);
+            this.txtPhone.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "First Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Last Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(50, 208);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "PhoneNbr";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(265, 261);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 10;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.BtnUpdate_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(140, 261);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 11;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.BtnSave_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(50, 350);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(18, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "ID";
+            // 
+            // txtIDremove
+            // 
+            this.txtIDremove.Location = new System.Drawing.Point(130, 347);
+            this.txtIDremove.Name = "txtIDremove";
+            this.txtIDremove.Size = new System.Drawing.Size(100, 20);
+            this.txtIDremove.TabIndex = 13;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(265, 344);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 14;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.txtIDremove);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.btnFind);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtIDremove;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ ContactsApp/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ContactsApp
+{
+    public partial class Form1 : Form
+    {
+
+        string ConnectionStr = " server = INSTRUCTORIT; database = CONTACTSAPP; user ID = ProfileUser; password = ProfileUser2019";
+        string sqlCommand;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnFind_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection myConnection = new SqlConnection(ConnectionStr))//connection with database
+                {
+                    myConnection.Open();
+                    sqlCommand = "SELECT * FROM Contacts WHERE ID =" + txtID.Text.ToString();//sql command
+                    SqlCommand myCommand = new SqlCommand(sqlCommand, myConnection);//connection with sql 
+                    SqlDataReader myReader = myCommand.ExecuteReader();//to select data
+                    while (myReader.Read())//while read, the program reads as an array
+                    {
+                        txtFirstName.Text = myReader["FirstName"].ToString();
+                        txtLastName.Text = myReader["LastName"].ToString();
+                        txtPhone.Text = myReader["PhoneNbr"].ToString();
+                    }
+                    myReader.Close();
+                    myConnection.Close();
+                }
+                    
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using(SqlConnection myConn = new SqlConnection(ConnectionStr))
+                {
+                    myConn.Open();
+                    sqlCommand = "INSERT INTO Contacts" + "( FirstName ,LastName ,PhoneNbr ) VALUES ('" + txtFirstName.Text.ToString() + "', '" + txtLastName.Text.ToString() + "' , '" + txtPhone.Text.ToString() + "');";
+                    SqlCommand myCommand = new SqlCommand(sqlCommand, myConn);
+                    myCommand.ExecuteNonQuery(); // when i dont need data back
+                    myConn.Close();
+                    MessageBox.Show("Add new friend successful");
+                }
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection myConn = new SqlConnection(ConnectionStr))
+                {
+                    myConn.Open();
+                    sqlCommand = "UPDATE Contacts SET FirstName= '" + txtFirstName.Text.ToString() + "', LastName='" + txtLastName.Text.ToString() + "', PhoneNbr=" + txtPhone.Text.ToString() + " WHERE ID="+txtID.Text.ToString();
+                    SqlCommand myCommand = new SqlCommand(sqlCommand, myConn);
+                    myCommand.ExecuteNonQuery();
+                    myConn.Close();
+                    MessageBox.Show("Update a friend successful");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection myConn = new SqlConnection(ConnectionStr))
+                {
+                    myConn.Open();
+                    sqlCommand = "DELETE FROM Contacts WHERE ID = " + txtIDremove.Text.ToString()+ ";";
+                    SqlCommand myCommand = new SqlCommand(sqlCommand, myConn);
+                    myCommand.ExecuteNonQuery();
+                    myConn.Close();
+                    MessageBox.Show("Delete a friend successful");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
